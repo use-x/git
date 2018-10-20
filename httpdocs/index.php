edf1cfc21426c854baad79b2df8406eeab46287c
use-x <emil-asmussen@use-x.com>
2018-10-20 13:50:35 +0200

    1. Commit nach Initialisierung auf dem lokalen Rechner ...

---
+++ httpdocs/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<title> Git </title>
+<link rel="stylesheet" href="/styles.css" />
+<style type="text/css">
+
+</style>
+<script type="text/JavaScript" src="/power.js"></script>
+<script type="text/JavaScript">
+
+</script>
+</head>
+<body>
+	<p>Hallo Emil</p>
+</body>
+</html>
